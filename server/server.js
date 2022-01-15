5833847d86173c8a92718daf7a280eab9b3d07b2
Luciano Matsuo <matsuoluciano@gmail.com>
2022-01-15 20:54:53 -0300

    configuring mongoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 import config from './../config/config'
 import app from './express'
+import mongoose from 'mongoose'
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.on('error', () => {
+    throw new Error(`Unable to connect to database: ${config.mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
     if (err) {
@@ -8,6 +16,3 @@ app.listen(config.port, (err) => {
     console.log('Server started on port %s.', config.port)
 })
 
-app.get('/', (req, res)=>{
-    res.status(200).send('Hola Luchin a')
-})
