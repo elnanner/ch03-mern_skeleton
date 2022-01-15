ce429d3c172e293b349562d751d49e11538d9061
Luciano Martín Matsuo <lmatsuo@santandertecnologia.com.ar>
2022-01-15 17:30:23 -0300

    Subida inicial

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import config from './../config/config'
+import app from './express'
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Server started on port %s.', config.port)
+})
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Hola Luchin')
+})
